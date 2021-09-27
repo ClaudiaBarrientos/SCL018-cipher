672095a7e2aff742a3df7077ad54b358fb4128fe
ClaudiaBarrientos <claudybz@gmail.com>
2021-09-27 12:02:06 -0300

    subir cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
+    encode: (displacementNumber, firstText) => {
+        console.log(displacementNumber);
+        console.log(firstText);
+
+
+        let encodedText = "";
+        for (let i = 0; i < firstText.length; i++) {
+
+            //Declarar variable número ascii para dar el charCodeAt a la constante firstText
+            let asciiNumber = firstText.charCodeAt(i);
+
+            if (asciiNumber >= 65 && asciiNumber <= 90) {
+                let capitalLetters = (asciiNumber - 65 + parseInt(displacementNumber)) % 26 + 65;
+                encodedText += String.fromCharCode(capitalLetters);
+            } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+                let lowerLetters = (asciiNumber - 97 + parseInt(displacementNumber)) % 26 + 97;
+                encodedText += String.fromCharCode(lowerLetters);
+            }
+        }
+        return encodedText
+    },
+
+
+    decode: (displacementNumber, firstText) => {
+
+
+        let decodedText = "";
+        for (let i = 0; i < firstText.length; i++) {
+            let asciiNumber = firstText.charCodeAt(i);
+
+            if (asciiNumber >= 65 && asciiNumber <= 90) {
+                let capitalLetters = (asciiNumber - 90 - parseInt(displacementNumber)) % 26 + 90;
+                decodedText += String.fromCharCode(capitalLetters);
+
+            } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+                let lowerLetters = (asciiNumber - 122 - parseInt(displacementNumber)) % 26 + 122;
+                decodedText += String.fromCharCode(lowerLetters);
+            }
+
+        }
+        return decodedText
+
+    }
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import cipher from './cipher.js';
+//Declarar variable para llamar al id mensaje a cifrar 
+const wordsToEncode = document.getElementById("message1");
+console.log(wordsToEncode);
 
-console.log(cipher);
+//Declarar variable para llamar al textarea de salida
+const finalMessage = document.getElementById("encryptedMessage");
+
+//Declarar evento listener del botón encode
+const encode = document.getElementById("encode");
+encode.addEventListener("click", () => {
+    const displacementNumber = document.getElementById("offset").value;
+    const firstText = document.getElementById("message1").value;
+
+    finalMessage.value = cipher.encode(displacementNumber, firstText);
+    console.log(encode);
+});
+
+//Declarar variable para llamar al id mensaje a descifrar
+const wordsToDecode = document.getElementById("message1");
+console.log(wordsToDecode);
+
+//Declarar variable para llamar al textarea de salida
+const finalMessage = document.getElementById("encryptedMessage");
+
+//Declarar evento listener del botón decode
+const decode = document.getElementById("decode");
+decode.addEventListener("click", () => {
+    const displacementNumber = document.getElementById("offset").value;
+    const firstText = document.getElementById("message1").value;
+    finalMessage.value = cipher.decode(displacementNumber, firstText);
+});
+
+//Declarar Función botón borrar
+const eraseBtn = document.getElementById("eraseButton");
+eraseBtn.addEventListener("click", () => {
+    document.getElementById("offset").value = "";
+    document.getElementById("message1").value = "";
+    document.getElementById("encryptedMessage").innerHTML = "";
+});
+
+import cipher from './cipher.js';
